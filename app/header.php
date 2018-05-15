08e20416461e464bf234f2a82520f9f6b832a685
OlegPuzenko <makesoft@mail.ru>
2018-05-15 10:55:45 +0300

    init

---
+++ app/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Metahash</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=1280" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" href="./static/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="./static/css/app.css" />
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+<div class="col-sm-8 page-col">
